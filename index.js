0cf4686c48ad591576635418d5f151bbed5d10a8
RamshackleJamathon <josephvandrunen@gmail.com>
2017-11-12 14:33:42 -0500

    fallback to html video stream, simplify interface

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
 var webcam = function(opts) {
   if (!opts) opts = {};
-  this.stream = undefined;
-  this.hasUserMedia = false;
   this.video = document.createElement("video");
-  this.source = document.createElement("source");
-  this.audioSource = opts.audioSource || undefined;
-  this.videoSource = opts.videoSource || undefined;
-  this.minWidth = opts.minWidth || 1280;
-  this.minHeight = opts.minHeight || 720;
-  this.minFrameRate = opts.minFrameRate || 60;
 };
 
 navigator.getUserMedia =
   navigator.getUserMedia ||
   navigator.webkitGetUserMedia ||
-  navigator.mozGetUserMedia ||
   navigator.msGetUserMedia;
 
+webcam.prototype.fallBack = function() {
+  this.video.crossOrigin = "anonymous";
+  this.video.loop = true;
+  this.video.src = "https://i.imgur.com/RoRyYhy.mp4";
+  this.video.type = "video/mp4";
+  this.video.play();
+};
+
 webcam.prototype.handleUserMedia = function(stream) {
-  this.source = stream;
   if (navigator.mozGetUserMedia) {
     this.video.mozSrcObject = stream;
   } else {
@@ -30,7 +28,7 @@ webcam.prototype.handleUserMedia = function(stream) {
   this.hasUserMedia = true;
 };
 
-webcam.prototype.sourceSelected = function() {
+webcam.prototype.requestUserMedia = function() {
   var constraints = {
     audio: false,
     video: { width: 1280, height: 720 }
@@ -42,7 +40,7 @@ webcam.prototype.sourceSelected = function() {
         this.handleUserMedia(stream);
       })
       .catch(error => {
-        console.error(error);
+        this.fallBack();
       });
   } else {
     navigator.getUserMedia(
@@ -51,44 +49,9 @@ webcam.prototype.sourceSelected = function() {
         this.handleUserMedia(stream);
       },
       error => {
-        console.error(error);
+        this.fallBack();
       }
     );
-  }
-};
-
-webcam.prototype.requestUserMedia = function() {
-  if (this.audioSource && this.videoSource) {
-    this.sourceSelected();
-  } else {
-    if ("mediaDevices" in navigator) {
-      navigator.mediaDevices
-        .enumerateDevices()
-        .then(devices => {
-          devices.forEach(device => {
-            if (device.kind === "audio") {
-              this.audioSource = device.id;
-            } else if (device.kind === "video") {
-              this.videoSource = device.id;
-            }
-          });
-          this.sourceSelected();
-        })
-        .catch(error => {
-          console.log(`${error.name}: ${error.message}`); // eslint-disable-line no-console
-        });
-    } else {
-      MediaStreamTrack.getSources(sources => {
-        sources.forEach(source => {
-          if (source.kind === "audio") {
-            this.audioSource = source.id;
-          } else if (source.kind === "video") {
-            this.videoSource = source.id;
-          }
-        });
-        this.sourceSelected();
-      });
-    }
   }
 };
 
